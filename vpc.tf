2a0fad11d23dc6262fe70355b922871ea0738966
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-11 21:12:33 +1100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,77 @@
+# Provider
+provider "aws" {
+  region = "ap-southeast-2"
+}
 
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "demo_vpc"
+  }
+}
+
+# IGW for demo_vpc
+resource "aws_internet_gateway" "vpc_igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_vpc_igw"
+  }
+}
+
+# Subnets in demo_vpc
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.subnets_cidr)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "demo_vpc_public_subnet_${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count                   = length(var.subnets_cidr)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.private_subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "demo_vpc_private_subnet_${count.index + 1}"
+  }
+}
+
+# Route table for demo_vpc
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_igw.id
+  }
+  tags = {
+    Name = "demo_vpc_public_rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_vpc_private_rt"
+  }
+}
+
+# Route table and subnets association for public subnet
+resource "aws_route_table_association" "public_rt_association" {
+  count          = length(var.subnets_cidr)
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Route table and subnets association for private subnet
+resource "aws_route_table_association" "private_rt_association" {
+  count          = length(var.private_subnets_cidr)
+  subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
