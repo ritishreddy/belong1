5e9a8f3d347570f265c07b3f09e94b8968941c5a
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-11 21:24:14 +1100

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+sudo yum update -y
+sudo yum install wget unzip httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+wget https://belong-coding-challenge.s3-ap-southeast-2.amazonaws.com/belong-test.html
+sudo mv belong-test.html /var/www/html/
+sudo systemctl restart httpd
