1179d9c9de8c37aa77e5a64524b100984b7b164e
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-12 17:41:41 +1100

    Update key-pair.tf

--- key-pair.tf
+++ key-pair.tf
@@ -3,10 +3,7 @@ resource "tls_private_key" "ssh" {
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "ssh_key" {
-  key_name   = "DemoMachine"
-  public_key = tls_private_key.ssh.public_key_openssh
-}
+
 
 output "ssh_private_key_pem" {
   value = tls_private_key.ssh.private_key_pem
