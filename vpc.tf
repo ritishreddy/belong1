2c1cdf9b093dede3a1c06b68a4eb00c15646cff9
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-12 16:26:30 +1100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,7 @@ resource "aws_key_pair" "ssh" {
   public_key = tls_private_key.ssh.public_key_openssh
 }
 # IGW for demo_vpc
-resource "aws_internet_gateway" "vpc_igw" {
+resource "aws_internet_gateway" "nat_gateway" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     Name = "demo_vpc_igw"
@@ -24,14 +24,13 @@ resource "aws_internet_gateway" "vpc_igw" {
 }
 
 # Subnets in demo_vpc
-resource "aws_subnet" "public_subnets" {
+resource "aws_subnet" "nat_gateway" {
   count                   = length(var.subnets_cidr)
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = element(var.subnets_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
   tags = {
-    Name = "demo_vpc_public_subnet_${count.index + 1}"
+    Name = "demo_vpc_nat_gateway_${count.index + 1}"
   }
 }
 
@@ -47,11 +46,11 @@ resource "aws_subnet" "private_subnets" {
 }
 
 # Route table for demo_vpc
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "nat_gateway" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vpc_igw.id
+    gateway_id = aws_internet_gateway.nat_gateway.id
   }
   tags = {
     Name = "demo_vpc_public_rt"
@@ -66,7 +65,7 @@ resource "aws_route_table" "private_rt" {
 }
 
 # Route table and subnets association for public subnet
-resource "aws_route_table_association" "public_rt_association" {
+resource "aws_route_table_association" "nat_gateway" {
   count          = length(var.subnets_cidr)
   subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
